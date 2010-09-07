c381c6cd8fcee7605be077c96e1e7ab92ec5d613
sebster <sebster@eff34587-6322-df11-911a-000c295dbd0b>
2010-09-07 22:53:50 +0000

    Added 5.5 bb, 25 bb, and 50 bb.
    git-svn-id: http://svn.poker.sebster.com/trunk/poker@89 eff34587-6322-df11-911a-000c295dbd0b

--- poker-core/src/main/java/com/sebster/poker/holdem/headsup/NashEquilibria.java
+++ poker-core/src/main/java/com/sebster/poker/holdem/headsup/NashEquilibria.java
@@ -112,6 +112,14 @@ public class NashEquilibria {
 		sbNashEquilibria.put(new Rational(5), sbStrategy);
 		bbNashEquilibria.put(new Rational(5), bbStrategy);
 
+		// 5.5 bb
+		sb = "{22=1, T2s=1, J2s=1, Q2s=1, K2s=1, A2s=1, 33=1, 43s=1, 53s=1, T3s=1, J3s=1, Q3s=1, K3s=1, A3s=1, 44=1, 54s=1, 64s=1, 74s=1, 84s=1, 94s=1, T4s=1, J4s=1, Q4s=1, K4s=1, A4s=1, 55=1, 65s=1, 75s=1, 85s=1, 95s=1, T5s=1, J5s=1, Q5s=1, K5s=1, A5s=1, 66=1, 76s=1, 86s=1, 96s=1, T6s=1, J6s=1, Q6s=1, K6s=1, A6s=1, 76o=1, 77=1, 87s=1, 97s=1, T7s=1, J7s=1, Q7s=1, K7s=1, A7s=1, 86o=1, 87o=1, 88=1, 98s=1, T8s=1, J8s=1, Q8s=1, K8s=1, A8s=1, 97o=1, 98o=1, 99=1, T9s=1, J9s=1, Q9s=1, K9s=1, A9s=1, T6o=1968894/2346577, T7o=1, T8o=1, T9o=1, TT=1, JTs=1, QTs=1, KTs=1, ATs=1, J5o=1, J6o=1, J7o=1, J8o=1, J9o=1, JTo=1, JJ=1, QJs=1, KJs=1, AJs=1, Q2o=1, Q3o=1, Q4o=1, Q5o=1, Q6o=1, Q7o=1, Q8o=1, Q9o=1, QTo=1, QJo=1, QQ=1, KQs=1, AQs=1, K2o=1, K3o=1, K4o=1, K5o=1, K6o=1, K7o=1, K8o=1, K9o=1, KTo=1, KJo=1, KQo=1, KK=1, AKs=1, A2o=1, A3o=1, A4o=1, A5o=1, A6o=1, A7o=1, A8o=1, A9o=1, ATo=1, AJo=1, AQo=1, AKo=1, AA=1}";
+		bb = "{22=1, J2s=1, Q2s=1, K2s=1, A2s=1, 33=1, J3s=1, Q3s=1, K3s=1, A3s=1, 44=1, J4s=1, Q4s=1, K4s=1, A4s=1, 55=1, J5s=1, Q5s=1, K5s=1, A5s=1, 66=1, 86s=1, 96s=1, T6s=1, J6s=1, Q6s=1, K6s=1, A6s=1, 77=1, 87s=1, 97s=1, T7s=1, J7s=1, Q7s=1, K7s=1, A7s=1, 88=1, 98s=1, T8s=1, J8s=1, Q8s=1, K8s=1, A8s=1, 98o=1, 99=1, T9s=1, J9s=1, Q9s=1, K9s=1, A9s=1, T7o=2057219/2346577, T8o=1, T9o=1, TT=1, JTs=1, QTs=1, KTs=1, ATs=1, J7o=1, J8o=1, J9o=1, JTo=1, JJ=1, QJs=1, KJs=1, AJs=1, Q2o=1, Q3o=1, Q4o=1, Q5o=1, Q6o=1, Q7o=1, Q8o=1, Q9o=1, QTo=1, QJo=1, QQ=1, KQs=1, AQs=1, K2o=1, K3o=1, K4o=1, K5o=1, K6o=1, K7o=1, K8o=1, K9o=1, KTo=1, KJo=1, KQo=1, KK=1, AKs=1, A2o=1, A3o=1, A4o=1, A5o=1, A6o=1, A7o=1, A8o=1, A9o=1, ATo=1, AJo=1, AQo=1, AKo=1, AA=1}";
+		sbStrategy = new ReadOnlyAllinOrFoldStrategy(MixedAllinOrFoldStrategy.fromString(sb));
+		bbStrategy = new ReadOnlyAllinOrFoldStrategy(MixedAllinOrFoldStrategy.fromString(bb));
+		sbNashEquilibria.put(new Rational(11, 2), sbStrategy);
+		bbNashEquilibria.put(new Rational(11, 2), bbStrategy);
+
 		// 6 bb
 		sb = "{22=1, T2s=1, J2s=1, Q2s=1, K2s=1, A2s=1, 33=1, 43s=1, 53s=1, T3s=1, J3s=1, Q3s=1, K3s=1, A3s=1, 44=1, 54s=1, 64s=1, 74s=1, 84s=1, T4s=1, J4s=1, Q4s=1, K4s=1, A4s=1, 55=1, 65s=1, 75s=1, 85s=1, 95s=1, T5s=1, J5s=1, Q5s=1, K5s=1, A5s=1, 66=1, 76s=1, 86s=1, 96s=1, T6s=1, J6s=1, Q6s=1, K6s=1, A6s=1, 76o=1, 77=1, 87s=1, 97s=1, T7s=1, J7s=1, Q7s=1, K7s=1, A7s=1, 86o=9055669/9143474, 87o=1, 88=1, 98s=1, T8s=1, J8s=1, Q8s=1, K8s=1, A8s=1, 97o=1, 98o=1, 99=1, T9s=1, J9s=1, Q9s=1, K9s=1, A9s=1, T7o=1, T8o=1, T9o=1, TT=1, JTs=1, QTs=1, KTs=1, ATs=1, J5o=1, J6o=1, J7o=1, J8o=1, J9o=1, JTo=1, JJ=1, QJs=1, KJs=1, AJs=1, Q2o=1, Q3o=1, Q4o=1, Q5o=1, Q6o=1, Q7o=1, Q8o=1, Q9o=1, QTo=1, QJo=1, QQ=1, KQs=1, AQs=1, K2o=1, K3o=1, K4o=1, K5o=1, K6o=1, K7o=1, K8o=1, K9o=1, KTo=1, KJo=1, KQo=1, KK=1, AKs=1, A2o=1, A3o=1, A4o=1, A5o=1, A6o=1, A7o=1, A8o=1, A9o=1, ATo=1, AJo=1, AQo=1, AKo=1, AA=1}";
 		bb = "{22=1, Q2s=1, K2s=1, A2s=1, 33=1, Q3s=1, K3s=1, A3s=1, 44=1, J4s=1, Q4s=1, K4s=1, A4s=1, 55=1, J5s=1, Q5s=1, K5s=1, A5s=1, 66=1, T6s=1, J6s=1, Q6s=1, K6s=1, A6s=1, 77=1, 87s=1, 97s=1, T7s=1, J7s=1, Q7s=1, K7s=1, A7s=1, 88=1, 98s=1, T8s=1, J8s=1, Q8s=1, K8s=1, A8s=1, 98o=4567801/4571737, 99=1, T9s=1, J9s=1, Q9s=1, K9s=1, A9s=1, T8o=1, T9o=1, TT=1, JTs=1, QTs=1, KTs=1, ATs=1, J7o=1, J8o=1, J9o=1, JTo=1, JJ=1, QJs=1, KJs=1, AJs=1, Q4o=1, Q5o=1, Q6o=1, Q7o=1, Q8o=1, Q9o=1, QTo=1, QJo=1, QQ=1, KQs=1, AQs=1, K2o=1, K3o=1, K4o=1, K5o=1, K6o=1, K7o=1, K8o=1, K9o=1, KTo=1, KJo=1, KQo=1, KK=1, AKs=1, A2o=1, A3o=1, A4o=1, A5o=1, A6o=1, A7o=1, A8o=1, A9o=1, ATo=1, AJo=1, AQo=1, AKo=1, AA=1}";
@@ -184,6 +192,14 @@ public class NashEquilibria {
 		sbNashEquilibria.put(new Rational(20), sbStrategy);
 		bbNashEquilibria.put(new Rational(20), bbStrategy);
 
+		// 25 bb
+		sb = "{22=1, A2s=1, 33=1, A3s=1, 44=1, K4s=1, A4s=1, 55=1, 65s=1, K5s=1, A5s=1, 66=1, 76s=1, 86s=1, 96s=1, Q6s=1, K6s=1, A6s=1, 77=1, 87s=1, 97s=1, T7s=1, J7s=1, Q7s=1, K7s=1, A7s=1, 88=1, 98s=1, T8s=1, J8s=1, Q8s=1, K8s=1, A8s=1, 99=1, T9s=1, J9s=1, Q9s=1, K9s=1, A9s=1, T9o=1, TT=1, JTs=1, QTs=1, KTs=1, ATs=1, J9o=1, JTo=1, JJ=1, QJs=1, KJs=1, AJs=1, QTo=1, QJo=1, QQ=1, KQs=1, AQs=1, KTo=1, KJo=1, KQo=1, KK=1, AKs=1, A2o=1, A3o=1, A4o=1, A5o=1, A6o=1, A7o=1, A8o=1, A9o=1, ATo=1, AJo=1, AQo=1, AKo=1, AA=1}";
+		bb = "{44=1, A4s=1, 55=1, A5s=1, 66=1, A6s=1, 77=1, A7s=1, 88=1, A8s=1, 99=1, A9s=1, TT=1, KTs=1, ATs=1, JJ=1, QJs=1, KJs=1, AJs=1, QQ=1, KQs=1, AQs=1, KJo=1, KQo=1, KK=1, AKs=1, A7o=1, A8o=1, A9o=1, ATo=1, AJo=1, AQo=1, AKo=1, AA=1}";
+		sbStrategy = new ReadOnlyAllinOrFoldStrategy(MixedAllinOrFoldStrategy.fromString(sb));
+		bbStrategy = new ReadOnlyAllinOrFoldStrategy(MixedAllinOrFoldStrategy.fromString(bb));
+		sbNashEquilibria.put(new Rational(25), sbStrategy);
+		bbNashEquilibria.put(new Rational(25), bbStrategy);
+
 		// 30 bb
 		sb = "{22=1, A2s=1, 33=1, A3s=1, 44=1, A4s=1, 55=1, K5s=1, A5s=1, 66=1, 76s=1, 86s=1, K6s=1, A6s=1, 77=1, 87s=1, 97s=1, T7s=1, J7s=1, K7s=1, A7s=1, 88=1, 98s=1, T8s=1, J8s=1, Q8s=1, K8s=1, A8s=1, 99=1, T9s=1, J9s=1, Q9s=1, K9s=1, A9s=1, T9o=1, TT=1, JTs=1, QTs=1, KTs=1, ATs=1, JTo=1, JJ=1, QJs=1, KJs=1, AJs=1, QTo=1, QJo=1, QQ=1, KQs=1, AQs=1, KTo=1, KJo=1, KQo=1, KK=1, AKs=1, A3o=11957599/15391806, A4o=1, A5o=1, A6o=1, A7o=1, A8o=1, A9o=1, ATo=1, AJo=1, AQo=1, AKo=1, AA=1}";
 		bb = "{44=1, 55=1, A5s=38315/72262, 66=1, A6s=1, 77=1, A7s=1, 88=1, A8s=1, 99=1, A9s=1, TT=1, KTs=1, ATs=1, JJ=1, KJs=1, AJs=1, QQ=1, KQs=1, AQs=1, KQo=1, KK=1, AKs=1, A8o=1, A9o=1, ATo=1, AJo=1, AQo=1, AKo=1, AA=1}";
@@ -191,6 +207,15 @@ public class NashEquilibria {
 		bbStrategy = new ReadOnlyAllinOrFoldStrategy(MixedAllinOrFoldStrategy.fromString(bb));
 		sbNashEquilibria.put(new Rational(30), sbStrategy);
 		bbNashEquilibria.put(new Rational(30), bbStrategy);
+
+		// 50 bb
+		sb = "{22=1, 33=1, A3s=1, 44=1, A4s=1, 55=1, A5s=1, 66=1, A6s=1, 77=1, A7s=1, 88=1, 98s=1, T8s=1, Q8s=1, K8s=1, A8s=1, 99=1, T9s=1, J9s=1, Q9s=1, K9s=1, A9s=1, TT=1, JTs=1, QTs=1, KTs=1, ATs=1, JJ=1, QJs=1, KJs=1, AJs=1, QJo=1, QQ=1, KQs=1, AQs=1, KTo=79677800/193976393, KJo=1, KQo=1, KK=1, AKs=1, ATo=1, AJo=1, AQo=1, AKo=1, AA=1}";
+		bb = "{66=1, 77=1, 88=1, 99=1, TT=1, ATs=1, JJ=1, AJs=1, QQ=1, KQs=1, AQs=1, KK=1, AKs=1, ATo=111413979/193976393, AJo=1, AQo=1, AKo=1, AA=1}";
+		sbStrategy = new ReadOnlyAllinOrFoldStrategy(MixedAllinOrFoldStrategy.fromString(sb));
+		bbStrategy = new ReadOnlyAllinOrFoldStrategy(MixedAllinOrFoldStrategy.fromString(bb));
+		sbNashEquilibria.put(new Rational(50), sbStrategy);
+		bbNashEquilibria.put(new Rational(50), bbStrategy);
+
 	}
 	
 }
