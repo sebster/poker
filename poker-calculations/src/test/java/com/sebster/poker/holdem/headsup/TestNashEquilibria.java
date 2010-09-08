cd2eade8e38f27d97c1cba05c872d30df7b73a08
sebster <sebster@eff34587-6322-df11-911a-000c295dbd0b>
2010-09-08 14:07:47 +0000

    Moved and converted to unit test.
    git-svn-id: http://svn.poker.sebster.com/trunk/poker@90 eff34587-6322-df11-911a-000c295dbd0b

---
+++ poker-calculations/src/test/java/com/sebster/poker/holdem/headsup/TestNashEquilibria.java
@@ -0,0 +1,35 @@
+package com.sebster.poker.holdem.headsup;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.sebster.math.rational.Rational;
+import com.sebster.poker.holdem.AllinOrFoldStrategy;
+import com.sebster.poker.solver.Solver;
+
+public class TestNashEquilibria {
+
+	@Test
+	public void testNashEquilibria() {
+		for (final Rational r : NashEquilibria.getRs()) {
+			final AllinOrFoldStrategy sbStrategy = NashEquilibria.getSBNashEquilibrium(r);
+			final AllinOrFoldStrategy bbStrategy = NashEquilibria.getBBNashEqulibrium(r);
+			final AllinOrFoldStrategy optBBStrategy = Solver.optimalBBStrategy(sbStrategy, r);
+			final AllinOrFoldStrategy optSBStrategy = Solver.optimalSBStrategy(bbStrategy, r);
+			System.out.println("Effective stacks = " + r + ", big blind = " + 1 + ", R = " + r + " (" + r.decimalValue() + ")");
+			final Rational nashEv = NashEquilibria.getSBNashEquilibriumEV(r);
+			final Rational computedEv = Solver.computeSBEV(sbStrategy, bbStrategy, r);
+			final Rational optSBEv = Solver.computeSBEV(sbStrategy, optBBStrategy, r);
+			final Rational optBBEv = Solver.computeSBEV(optSBStrategy, bbStrategy, r);
+			System.out.println("Specified EV      = " + nashEv + " (" + nashEv.decimalValue() + ")");
+			System.out.println("Computed EV       = " + computedEv + " (" + computedEv.decimalValue() + ")");
+			System.out.println("EV against opt BB = " + optSBEv + " (" + optSBEv.decimalValue() + ")");
+			System.out.println("EV against opt SB = " + optBBEv + " (" + optBBEv.decimalValue() + ")");
+			Assert.assertEquals(nashEv, computedEv); // Verify registered EV is correct.
+			Assert.assertEquals(nashEv, optSBEv);    // Verify registered EV is optimal against any SB strategy.
+			Assert.assertEquals(nashEv, optBBEv);    // Verify registered EV is optimal against any BB strategy.
+		}
+	}
+
+}
