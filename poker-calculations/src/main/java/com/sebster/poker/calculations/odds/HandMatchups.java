6fd52adac393152efcbc7325c99be2b2cdcc23fd
sebster <sebster@eff34587-6322-df11-911a-000c295dbd0b>
2010-09-13 21:30:23 +0000

    Fixed and added hand matchups.
    git-svn-id: http://svn.poker.sebster.com/trunk/poker@100 eff34587-6322-df11-911a-000c295dbd0b

--- poker-calculations/src/main/java/com/sebster/poker/calculations/odds/HandMatchups.java
+++ poker-calculations/src/main/java/com/sebster/poker/calculations/odds/HandMatchups.java
@@ -3,273 +3,176 @@ package com.sebster.poker.calculations.odds;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
+import com.sebster.math.rational.Rational;
 import com.sebster.poker.HoleCategory;
 import com.sebster.poker.holdem.odds.TwoPlayerPreFlopHoleCategoryOddsDB;
 import com.sebster.poker.odds.Odds;
 
 public class HandMatchups {
 
-	public static Object[] getOdds2OverVs2Under() {
-		double maxEquity = 0, minEquity = 1, totEquity = 0, totWeight = 0;
-		HoleCategory[] maxHoles = null, minHoles = null;
+	private static interface MatchupFilter {
+		boolean accept(final HoleCategory hc1, final HoleCategory hc2);
+	}
+	
+	public static class Result {
+		public final Rational minEquity;
+		public final HoleCategory minHoleCategory1;
+		public final HoleCategory minHoleCategory2;
+		public final Rational maxEquity;
+		public final HoleCategory maxHoleCategory1;
+		public final HoleCategory maxHoleCategory2;
+		public final Rational avgEquity;
+		
+		public Result(Rational minEquity, HoleCategory minHoleCategory1, HoleCategory minHoleCategory2, Rational maxEquity, HoleCategory maxHoleCategory1, HoleCategory maxHoleCategory2, Rational avgEquity) {
+			this.minEquity = minEquity;
+			this.minHoleCategory1 = minHoleCategory1;
+			this.minHoleCategory2 = minHoleCategory2;
+			this.maxEquity = maxEquity;
+			this.maxHoleCategory1 = maxHoleCategory1;
+			this.maxHoleCategory2 = maxHoleCategory2;
+			this.avgEquity = avgEquity;
+		}
+		
+	}
+	
+	private static Result calculateMatchupStats(final MatchupFilter filter) {
+		Rational maxEquity = Rational.ZERO, minEquity = Rational.ONE, totEquity = Rational.ZERO, totWeight = Rational.ZERO;
+		HoleCategory maxHoleCategory1 = null, maxHoleCategory2 = null, minHoleCategory1 = null, minHoleCategory2 = null;
 		for (final HoleCategory hc1 : HoleCategory.values()) {
 			for (final HoleCategory hc2 : HoleCategory.values()) {
-				if (hc1.getLowRank().getValue() > hc2.getHighRank().getValue() && !hc1.isPair() && !hc2.isPair()) {
+				if (filter.accept(hc1, hc2)) {
 					// 2 overcards vs 2 undercards
 					final Odds odds = TwoPlayerPreFlopHoleCategoryOddsDB.getInstance().getOdds(hc1, hc2);
-					final double equity = odds.getEquity();
-					if (equity > maxEquity) {
+					final Rational equity = odds.getEquity();
+					if (equity.compareTo(maxEquity) > 0) {
 						maxEquity = equity;
-						maxHoles = new HoleCategory[] { hc1, hc2 };
+						maxHoleCategory1 = hc1;
+						maxHoleCategory2 = hc2;
 					}
-					if (equity < minEquity) {
+					if (equity.compareTo(minEquity) < 0) {
 						minEquity = equity;
-						minHoles = new HoleCategory[] { hc1, hc2 };
+						minHoleCategory1 = hc1;
+						minHoleCategory2 = hc2;
 					}
 					final int weight1 = hc1.getSize();
 					final int weight2 = hc2.getSize();
 					final int weight = weight1 * weight2;
 
-					totEquity += equity * weight;
-					totWeight += weight;
+					totEquity = totEquity.add(equity.multiply(weight));
+					totWeight = totWeight.add(weight);
 				}
 			}
 		}
-		return new Object[] { minEquity, minHoles, maxEquity, maxHoles, totEquity / totWeight };
+		return new Result(minEquity, minHoleCategory1, minHoleCategory2, maxEquity, maxHoleCategory1, maxHoleCategory2, totEquity.divide(totWeight));
 	}
-
-	public static Object[] getOddsOverUnderVs2Between() {
-		double maxEquity = 0, minEquity = 1, totEquity = 0, totWeight = 0;
-		HoleCategory[] maxHoles = null, minHoles = null;
-		for (final HoleCategory hc1 : HoleCategory.values()) {
-			for (final HoleCategory hc2 : HoleCategory.values()) {
-				if (hc1.getHighRank().getValue() > hc2.getHighRank().getValue() && hc2.getLowRank().getValue() > hc1.getLowRank().getValue() && !hc1.isPair() && !hc2.isPair()) {
-					// 1 over 1 under vs 2 in between.
-					final Odds odds = TwoPlayerPreFlopHoleCategoryOddsDB.getInstance().getOdds(hc1, hc2);
-					final double equity = odds.getEquity();
-					if (equity > maxEquity) {
-						maxEquity = equity;
-						maxHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					if (equity < minEquity) {
-						minEquity = equity;
-						minHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					final int weight1 = hc1.getSize();
-					final int weight2 = hc2.getSize();
-					final int weight = weight1 * weight2;
-
-					totEquity += equity * weight;
-					totWeight += weight;
-				}
+	
+	public static Result getOdds2OverVs2Under() {
+		return calculateMatchupStats(new MatchupFilter() {
+			@Override
+			public boolean accept(final HoleCategory hc1, final HoleCategory hc2) {
+				return hc1.getLowRank().getValue() > hc2.getHighRank().getValue() && !hc1.isPair() && !hc2.isPair();
 			}
-		}
-		return new Object[] { minEquity, minHoles, maxEquity, maxHoles, totEquity / totWeight };
+		});
 	}
-
-	public static Object[] getOddsOverVs1Between() {
-		double maxEquity = 0, minEquity = 1, totEquity = 0, totWeight = 0;
-		HoleCategory[] maxHoles = null, minHoles = null;
-		for (final HoleCategory hc1 : HoleCategory.values()) {
-			for (final HoleCategory hc2 : HoleCategory.values()) {
-				if (hc1.getHighRank().getValue() > hc2.getHighRank().getValue() && hc2.getHighRank().getValue() > hc1.getLowRank().getValue() && hc1.getLowRank().getValue() > hc2.getLowRank().getValue() && !hc1.isPair() && !hc2.isPair()) {
-					// 1 over vs 1 in between.
-					final Odds odds = TwoPlayerPreFlopHoleCategoryOddsDB.getInstance().getOdds(hc1, hc2);
-					final double equity = odds.getEquity();
-					if (equity > maxEquity) {
-						maxEquity = equity;
-						maxHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					if (equity < minEquity) {
-						minEquity = equity;
-						minHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					final int weight1 = hc1.getSize();
-					final int weight2 = hc2.getSize();
-					final int weight = weight1 * weight2;
-
-					totEquity += equity * weight;
-					totWeight += weight;
-				}
+	
+	public static Result getOddsOverUnderVs2Between() {
+		return calculateMatchupStats(new MatchupFilter() {
+			@Override
+			public boolean accept(final HoleCategory hc1, final HoleCategory hc2) {
+				return hc1.getHighRank().getValue() > hc2.getHighRank().getValue() && hc2.getLowRank().getValue() > hc1.getLowRank().getValue() && !hc1.isPair() && !hc2.isPair();
 			}
-		}
-		return new Object[] { minEquity, minHoles, maxEquity, maxHoles, totEquity / totWeight };
+		});
 	}
 
-	public static Object[] getOddsPairVs2Over() {
-		double maxEquity = 0, minEquity = 1, totEquity = 0, totWeight = 0;
-		HoleCategory[] maxHoles = null, minHoles = null;
-		for (final HoleCategory hc1 : HoleCategory.values()) {
-			for (final HoleCategory hc2 : HoleCategory.values()) {
-				if (hc1.getHighRank().getValue() < hc2.getLowRank().getValue() && hc1.isPair() && !hc2.isPair()) {
-					// 1 over vs 1 in between.
-					final Odds odds = TwoPlayerPreFlopHoleCategoryOddsDB.getInstance().getOdds(hc1, hc2);
-					final double equity = odds.getEquity();
-					if (equity > maxEquity) {
-						maxEquity = equity;
-						maxHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					if (equity < minEquity) {
-						minEquity = equity;
-						minHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					final int weight1 = hc1.getSize();
-					final int weight2 = hc2.getSize();
-					final int weight = weight1 * weight2;
-
-					totEquity += equity * weight;
-					totWeight += weight;
-				}
+	public static Result getOddsOverVs1Between() {
+		return calculateMatchupStats(new MatchupFilter() {
+			@Override
+			public boolean accept(final HoleCategory hc1, final HoleCategory hc2) {
+				return hc1.getHighRank().getValue() > hc2.getHighRank().getValue() && hc2.getHighRank().getValue() > hc1.getLowRank().getValue() && hc1.getLowRank().getValue() > hc2.getLowRank().getValue() && !hc1.isPair() && !hc2.isPair();
 			}
-		}
-		return new Object[] { minEquity, minHoles, maxEquity, maxHoles, totEquity / totWeight };
+		});
 	}
 
-	public static Object[] getOddsPairVs1Over() {
-		double maxEquity = 0, minEquity = 1, totEquity = 0, totWeight = 0;
-		HoleCategory[] maxHoles = null, minHoles = null;
-		for (final HoleCategory hc1 : HoleCategory.values()) {
-			for (final HoleCategory hc2 : HoleCategory.values()) {
-				if (hc1.getHighRank().getValue() < hc2.getHighRank().getValue() && hc1.getHighRank().getValue() >= hc2.getLowRank().getValue() && hc1.isPair() && !hc2.isPair()) {
-					// 1 over vs 1 in between.
-					final Odds odds = TwoPlayerPreFlopHoleCategoryOddsDB.getInstance().getOdds(hc1, hc2);
-					final double equity = odds.getEquity();
-					if (equity > maxEquity) {
-						maxEquity = equity;
-						maxHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					if (equity < minEquity) {
-						minEquity = equity;
-						minHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					final int weight1 = hc1.getSize();
-					final int weight2 = hc2.getSize();
-					final int weight = weight1 * weight2;
-
-					totEquity += equity * weight;
-					totWeight += weight;
-				}
+	public static Result getOddsPairVs2Over() {
+		return calculateMatchupStats(new MatchupFilter() {
+			@Override
+			public boolean accept(final HoleCategory hc1, final HoleCategory hc2) {
+				return hc1.getHighRank().getValue() < hc2.getLowRank().getValue() && hc1.isPair() && !hc2.isPair();
 			}
-		}
-		return new Object[] { minEquity, minHoles, maxEquity, maxHoles, totEquity / totWeight };
+		});
 	}
 
-	public static Object[] getOddsPairVs2Under() {
-		double maxEquity = 0, minEquity = 1, totEquity = 0, totWeight = 0;
-		HoleCategory[] maxHoles = null, minHoles = null;
-		for (final HoleCategory hc1 : HoleCategory.values()) {
-			for (final HoleCategory hc2 : HoleCategory.values()) {
-				if (hc1.getHighRank().getValue() > hc2.getHighRank().getValue() && hc1.isPair() && !hc2.isPair()) {
-					// 1 over vs 1 in between.
-					final Odds odds = TwoPlayerPreFlopHoleCategoryOddsDB.getInstance().getOdds(hc1, hc2);
-					final double equity = odds.getEquity();
-					if (equity > maxEquity) {
-						maxEquity = equity;
-						maxHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					if (equity < minEquity) {
-						minEquity = equity;
-						minHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					final int weight1 = hc1.getSize();
-					final int weight2 = hc2.getSize();
-					final int weight = weight1 * weight2;
-
-					totEquity += equity * weight;
-					totWeight += weight;
-				}
+	public static Result getOddsPairVs1Over1Under() {
+		return calculateMatchupStats(new MatchupFilter() {
+			@Override
+			public boolean accept(final HoleCategory hc1, final HoleCategory hc2) {
+				return hc1.getHighRank().getValue() < hc2.getHighRank().getValue() && hc1.getHighRank().getValue() > hc2.getLowRank().getValue() && hc1.isPair() && !hc2.isPair();
 			}
-		}
-		return new Object[] { minEquity, minHoles, maxEquity, maxHoles, totEquity / totWeight };
+		});
 	}
 
-	public static Object[] getOddsOverPairVsUnderPair() {
-		double maxEquity = 0, minEquity = 1, totEquity = 0, totWeight = 0;
-		HoleCategory[] maxHoles = null, minHoles = null;
-		for (final HoleCategory hc1 : HoleCategory.values()) {
-			for (final HoleCategory hc2 : HoleCategory.values()) {
-				if (hc1.getHighRank().getValue() > hc2.getHighRank().getValue() && hc1.isPair() && hc2.isPair()) {
-					// 1 over vs 1 in between.
-					final Odds odds = TwoPlayerPreFlopHoleCategoryOddsDB.getInstance().getOdds(hc1, hc2);
-					final double equity = odds.getEquity();
-					if (equity > maxEquity) {
-						maxEquity = equity;
-						maxHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					if (equity < minEquity) {
-						minEquity = equity;
-						minHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					final int weight1 = hc1.getSize();
-					final int weight2 = hc2.getSize();
-					final int weight = weight1 * weight2;
-
-					totEquity += equity * weight;
-					totWeight += weight;
-				}
+	public static Result getOddsPairVsDominatedLowCard() {
+		return calculateMatchupStats(new MatchupFilter() {
+			@Override
+			public boolean accept(final HoleCategory hc1, final HoleCategory hc2) {
+				return hc1.getHighRank().getValue() < hc2.getHighRank().getValue() && hc1.getHighRank().getValue() == hc2.getLowRank().getValue() && hc1.isPair() && !hc2.isPair();
 			}
-		}
-		return new Object[] { minEquity, minHoles, maxEquity, maxHoles, totEquity / totWeight };
+		});
 	}
 
-	public static Object[] getOddsDominatedLowCard() {
-		double maxEquity = 0, minEquity = 1, totEquity = 0, totWeight = 0;
-		HoleCategory[] maxHoles = null, minHoles = null;
-		for (final HoleCategory hc1 : HoleCategory.values()) {
-			for (final HoleCategory hc2 : HoleCategory.values()) {
-				if (hc1.getHighRank().getValue() == hc2.getHighRank().getValue() && hc1.getLowRank().getValue() > hc2.getLowRank().getValue() && !hc1.isPair() && !hc2.isPair()) {
-					// 1 over vs 1 in between.
-					final Odds odds = TwoPlayerPreFlopHoleCategoryOddsDB.getInstance().getOdds(hc1, hc2);
-					final double equity = odds.getEquity();
-					if (equity > maxEquity) {
-						maxEquity = equity;
-						maxHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					if (equity < minEquity) {
-						minEquity = equity;
-						minHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					final int weight1 = hc1.getSize();
-					final int weight2 = hc2.getSize();
-					final int weight = weight1 * weight2;
-
-					totEquity += equity * weight;
-					totWeight += weight;
-				}
+	public static Result getOddsPairVsDominatedHighCard() {
+		return calculateMatchupStats(new MatchupFilter() {
+			@Override
+			public boolean accept(final HoleCategory hc1, final HoleCategory hc2) {
+				return hc1.getHighRank().getValue() == hc2.getHighRank().getValue() && hc1.getHighRank().getValue() > hc2.getLowRank().getValue() && hc1.isPair() && !hc2.isPair();
 			}
-		}
-		return new Object[] { minEquity, minHoles, maxEquity, maxHoles, totEquity / totWeight };
+		});
 	}
 
-	public static Object[] getOddsDominatedHighCard() {
-		double maxEquity = 0, minEquity = 1, totEquity = 0, totWeight = 0;
-		HoleCategory[] maxHoles = null, minHoles = null;
-		for (final HoleCategory hc1 : HoleCategory.values()) {
-			for (final HoleCategory hc2 : HoleCategory.values()) {
-				if (hc1.getHighRank().getValue() > hc2.getHighRank().getValue() && hc1.getLowRank().getValue() == hc2.getLowRank().getValue() && !hc1.isPair() && !hc2.isPair()) {
-					// 1 over vs 1 in between.
-					final Odds odds = TwoPlayerPreFlopHoleCategoryOddsDB.getInstance().getOdds(hc1, hc2);
-					final double equity = odds.getEquity();
-					if (equity > maxEquity) {
-						maxEquity = equity;
-						maxHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					if (equity < minEquity) {
-						minEquity = equity;
-						minHoles = new HoleCategory[] { hc1, hc2 };
-					}
-					final int weight1 = hc1.getSize();
-					final int weight2 = hc2.getSize();
-					final int weight = weight1 * weight2;
-
-					totEquity += equity * weight;
-					totWeight += weight;
-				}
+	public static Result getOddsPairVs2Under() {
+		return calculateMatchupStats(new MatchupFilter() {
+			@Override
+			public boolean accept(final HoleCategory hc1, final HoleCategory hc2) {
+				return hc1.getHighRank().getValue() > hc2.getHighRank().getValue() && hc1.isPair() && !hc2.isPair();
 			}
-		}
-		return new Object[] { minEquity, minHoles, maxEquity, maxHoles, totEquity / totWeight };
+		});
+	}
+	
+	public static Result getOddsOverPairVsUnderPair() {
+		return calculateMatchupStats(new MatchupFilter() {
+			@Override
+			public boolean accept(final HoleCategory hc1, final HoleCategory hc2) {
+				return hc1.getHighRank().getValue() > hc2.getHighRank().getValue() && hc1.isPair() && hc2.isPair();
+			}
+		});
 	}
 
+	public static Result getOddsDominatedLowCard() {
+		return calculateMatchupStats(new MatchupFilter() {
+			@Override
+			public boolean accept(final HoleCategory hc1, final HoleCategory hc2) {
+				return hc1.getHighRank().getValue() == hc2.getHighRank().getValue() && hc1.getLowRank().getValue() > hc2.getLowRank().getValue() && !hc1.isPair() && !hc2.isPair();
+			}
+		});
+	}
+
+	public static Result getOddsDominatedHighCard() {
+		return calculateMatchupStats(new MatchupFilter() {
+			@Override
+			public boolean accept(final HoleCategory hc1, final HoleCategory hc2) {
+				return hc1.getHighRank().getValue() > hc2.getHighRank().getValue() && hc1.getLowRank().getValue() == hc2.getLowRank().getValue() && !hc1.isPair() && !hc2.isPair();
+			}
+		});
+	}
+
+	public static Result getOddsLowCardEqualsOtherHighCard() {
+		return calculateMatchupStats(new MatchupFilter() {
+			@Override
+			public boolean accept(final HoleCategory hc1, final HoleCategory hc2) {
+				return hc1.getLowRank().getValue() == hc2.getHighRank().getValue() && !hc1.isPair() && !hc2.isPair();
+			}
+		});
+	}
 
 	public static void main(final String[] args) {
 		System.out.println("2 over vs 2 under");
@@ -280,35 +183,40 @@ public class HandMatchups {
 		printResults(getOddsOverVs1Between());
 		System.out.println("pair vs 2 over");
 		printResults(getOddsPairVs2Over());
-		System.out.println("pair vs 1 over");
-		printResults(getOddsPairVs1Over());
 		System.out.println("pair vs 2 under");
 		printResults(getOddsPairVs2Under());
+		System.out.println("pair vs 1 over 1 under");
+		printResults(getOddsPairVs1Over1Under());
+		System.out.println("pair vs dominated low card");
+		printResults(getOddsPairVsDominatedLowCard());
+		System.out.println("pair vs dominated high card");
+		printResults(getOddsPairVsDominatedHighCard());
 		System.out.println("overpair vs underpair");
 		printResults(getOddsOverPairVsUnderPair());
 		System.out.println("dominated low card");
 		printResults(getOddsDominatedLowCard());
 		System.out.println("dominated high card");
 		printResults(getOddsDominatedHighCard());
+		System.out.println("low card equals other high card");
+		printResults(getOddsLowCardEqualsOtherHighCard());
 	}
 
-	private static void printResults(Object[] results) {
-		final Double minEquity = (Double) results[0], maxEquity = (Double) results[2], avgEquity = (Double) results[4];
-		final HoleCategory[] minHoles = (HoleCategory[]) results[1], maxHoles = (HoleCategory[]) results[3];
-		System.out.println("Minimum equity: " + equityToString(minEquity) + " with hole " + minHoles[0] + " vs " + minHoles[1]);
-		System.out.println("Maximum equity: " + equityToString(maxEquity) + " with hole " + maxHoles[0] + " vs " + maxHoles[1]);
-		System.out.println("Average equity: " + equityToString(avgEquity));
-
+	private static void printResults(final Result result) {
+		System.out.println("Minimum equity: " + equityToString(result.minEquity) + " with hole " + result.minHoleCategory1 + " vs " + result.minHoleCategory2);
+		System.out.println("Maximum equity: " + equityToString(result.maxEquity) + " with hole " + result.maxHoleCategory1 + " vs " + result.maxHoleCategory2);
+		System.out.println("Average equity: " + equityToString(result.avgEquity));
+		System.out.println();
 	}
 
-	private static String equityToString(final double equity) {
-		NumberFormat decimalFormat = new DecimalFormat("#.0");
+	private static String equityToString(final Rational equity) {
+		NumberFormat decimalFormat = new DecimalFormat("0.0");
 		StringBuffer buffer = new StringBuffer();
-		buffer.append(decimalFormat.format(equity * 100));
+		final double equityPercent = equity.multiply(100).doubleValue();
+		buffer.append(decimalFormat.format(equityPercent));
 		buffer.append("-");
-		buffer.append(decimalFormat.format((1 - equity) * 100));
+		buffer.append(decimalFormat.format(100 - equityPercent));
 		buffer.append(" (");
-		buffer.append(decimalFormat.format(equity / (1 - equity)));
+		buffer.append(decimalFormat.format(equityPercent / (100 - equityPercent)));
 		buffer.append("-1)");
 		return buffer.toString();
 	}
