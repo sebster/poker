9ec508cc89415d8569713d248d5b59de92601740
sebster <sebster@eff34587-6322-df11-911a-000c295dbd0b>
2010-10-22 14:53:56 +0000

    Some extra utils.
    git-svn-id: http://svn.poker.sebster.com/trunk/poker@116 eff34587-6322-df11-911a-000c295dbd0b

--- poker-core/src/main/java/com/sebster/math/MathUtil.java
+++ poker-core/src/main/java/com/sebster/math/MathUtil.java
@@ -5,19 +5,39 @@ public final class MathUtil {
 	private MathUtil() {
 		// Utility class.
 	}
-	
-	public static <T extends Comparable<T>> T max(T first, T second) {
+
+	public static <T extends Comparable<? super T>> T min(T first, T second) {
+		if (first.compareTo(second) <= 0) {
+			return first;
+		}
+		return second;
+	}
+
+	public static <T extends Comparable<? super T>> T max(T first, T second) {
 		if (first.compareTo(second) >= 0) {
 			return first;
 		}
 		return second;
 	}
-	
-	public static <T extends Comparable<T>> T min(T first, T second) {
-		if (first.compareTo(second) <= 0) {
-			return first;
+
+	public static <T extends Comparable<? super T>> T min(final T first, final T... rest) {
+		T result = first;
+		for (T value : rest) {
+			if (value.compareTo(result) < 0) {
+				result = value;
+			}
 		}
-		return second;
+		return result;
+	}
+
+	public static <T extends Comparable<? super T>> T max(final T first, final T... rest) {
+		T result = first;
+		for (T value : rest) {
+			if (value.compareTo(result) > 0) {
+				result = value;
+			}
+		}
+		return result;
 	}
 
 }
